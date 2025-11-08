aba7b5491d99d196857874a47ad1356375d3a5b6
Nescalz <rostislavmatashok@gmail.com>
2025-11-09 01:13:44 +0300

    Update db func

--- app/models/database.py
+++ app/models/database.py
@@ -8,3 +8,11 @@ async def jsons(user_id):
                 row = await cursor.fetchone()
                 return row[0]
         
+async def delete(user_id):
+    async with connect("database.db") as db:
+        async with db.execute(
+            "SELECT json FROM Users WHERE telegram_id = ?", 
+            (user_id,)) as cursor:
+                row = await cursor.fetchone()
+                return row[0]
+        
